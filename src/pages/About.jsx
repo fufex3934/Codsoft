6b07b0f758b96d778f28d5126e090ab3b874100f
fufex3934 <fufawakgari174@gmail.com>
2025-04-10 17:36:57 +0300

    about me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div className='text-center p-10'>
       <h1 className='text-3xl font-bold'>About Me</h1>
-      <p className="mt-4 text-gray-600">Learn more about me here.</p>
+      <p className="mt-4 text-gray-600">I’m a React developer passionate about building beautiful interfaces.</p>
     </div>
   )
 }
