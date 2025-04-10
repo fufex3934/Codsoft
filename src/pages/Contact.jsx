442865b304b976a865c2d2e7dc4ecd24b60a42b9
fufex3934 <fufawakgari174@gmail.com>
2025-04-10 17:30:10 +0300

    contact me page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className='text-center p-10'>
+        <h1 className="text-3xl font-bold">Contact Me</h1>
+        <p className="mt-4 text-gray-600">Feel free to get in touch.</p>
+    </div>
   )
 }
