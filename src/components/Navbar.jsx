bcf6ae7b441e56bb1c99455189302c7eb138b984
fufex3934 <fufawakgari174@gmail.com>
2025-04-11 09:43:34 +0300

    navbar for desktop

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,24 @@
+import { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
 export default function Navbar() {
+  const [isOpen,setIsOpen]  = useState(false);
+  const toggleMenu = ()=>setIsOpen(!isOpen);
+
+  const navLinkClasses = ({isActive})=>isActive ? "text-blue-400":"hover:text-blue-300 transition"
   return (
-    <nav className="bg-gray-800 text-white">
+    <nav className="bg-gray-900 text-white shadow">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <Link to="/" className="text-xl font-bold">MyPortfolio</Link>
-        <div className="space-x-4">
-          <NavLink to="/" className={({ isActive }) => isActive ? "text-blue-400" : ""}>Home</NavLink>
-          <NavLink to="/projects" className={({ isActive }) => isActive ? "text-blue-400" : ""}>Projects</NavLink>
-          <NavLink to="/about" className={({ isActive }) => isActive ? "text-blue-400" : ""}>About</NavLink>
-          <NavLink to="/contact" className={({ isActive }) => isActive ? "text-blue-400" : ""}>Contact</NavLink>
+        <Link to={"/"} className='text-2xl font-bold'>Fufa Portifolio</Link>
+
+        {/* for Desktop */}
+        <div className='hidden md:flex space-x-6'>
+          <NavLink to={"/"} className={navLinkClasses}>Home</NavLink>
+          <NavLink to={"/projects"}className={navLinkClasses}>Projects</NavLink>
+          <NavLink to={"/about"} className={navLinkClasses}>About</NavLink>
+          <NavLink to={"/contact"} className={navLinkClasses}>Contact</NavLink>
         </div>
+        
       </div>
     </nav>
   )
